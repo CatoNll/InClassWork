7a7d752788a915bfd92f0b764c5433a13a129cba
noahl <noahllowe27@gmail.com>
2022-11-08 13:58:43 -0500

    - every thing

---
+++ src/Lesson_10_Chars.java
@@ -0,0 +1,27 @@
+public class Lesson_10_Chars {
+        public static void main(String[] args) {
+
+ /*
+        int letterVal = 'c';
+        System.out.println(letterVal);
+
+ */
+
+
+            String word = "Hello";
+
+            char char1 = ' ';
+
+            int letterVal = 0;
+
+
+
+
+            for (int i = 0; i < word.length(); i++) {
+                char1 = word.charAt(i);
+                letterVal = char1;
+
+
+            }
+    }
+}
