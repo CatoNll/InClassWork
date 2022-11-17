3bf4a7fb72000fb3f488f3207eaea3b2171182a0
Noah Lowe <noahllowe27@gmail.com>
2022-11-17 16:23:13 -0500

    Change Log
    - add new comments

--- src/Lesson_11_Game.java
+++ src/Lesson_11_Game.java
@@ -27,6 +27,10 @@ public class Lesson_11_Game {
         int cardAmount = 0;
         int playerCardsRule1 = 0;
         int computerCardsRule1 = 0;
+        int randompower = 0;
+        int round = 0;
+        int playerScore = 0;
+        int computerScore = 0;
 
 
         System.out.println("Welcome to Black Jack. Press Enter to play. Press q to exit. ");
@@ -37,8 +41,13 @@ public class Lesson_11_Game {
                 System.out.println("Thank you for playing!");
                 keepLooping = false;
             }
+            round++;
             System.out.println("How many cards do you want to play. (Recommended: 3 cards) ");
             cardAmount = scan.nextInt();
+            System.out.println("Round: " + round);
+            System.out.print("Player Score: " + playerScore);
+            System.out.print("                       Computer Score: " + computerScore);
+            System.out.println();
 
             int playerCards[] = new int[cardAmount];
             int computerCards[] = new int[cardAmount];
@@ -52,8 +61,6 @@ public class Lesson_11_Game {
 
 
 
-
-
             System.out.print("Your cards are: ");
             for (int i = 0; i < cardAmount; i++) {
                 System.out.print(playerCards[i] + " ");
@@ -62,9 +69,8 @@ public class Lesson_11_Game {
                 }
             }
 
-            System.out.println();
 
-            System.out.print("The computers cards are: ");
+            System.out.print("          The computers cards are: ");
             for (int i = 0; i < cardAmount; i++) {
                 System.out.print(computerCards[i] + " ");
                 if (i != computerCards.length-1){
@@ -76,7 +82,7 @@ public class Lesson_11_Game {
             System.out.println();
             // rule 1: the cards that add together by the power of 2 will win
             for (int i = 0; i < cardAmount; i++) {
-                int randompower = (int) (1 + Math.random()* 2.99);
+                randompower = (int) (1 + Math.random()* 2.99);
                 playerCards[i] = (int)Math.pow(playerCards[i], randompower);
                 computerCards[i] = (int)Math.pow(computerCards[i], randompower);
 
@@ -84,9 +90,25 @@ public class Lesson_11_Game {
                 computerCardsRule1 = computerCardsRule1 + computerCards[i];
             }
 
+            System.out.println();
             System.out.println("Your cards to the power of 2 then add together are: " + playerCardsRule1);
             System.out.println("The computers cards to the power of 2 then add together are: " + computerCardsRule1);
             System.out.println();
+
+            if (playerCardsRule1 > computerCardsRule1) {
+
+                System.out.println("You win this round");
+                System.out.println("You won because your cards added together after being calculated to the power of "
+                        + randompower + " was bigger then the computers cards.");
+                playerScore++;
+
+            } else {
+                System.out.println("The computer win this round");
+                System.out.println("The computer won because their cards added together after being calculated to the power of "
+                        + randompower + " was bigger then your cards.");
+                computerScore++;
+            }
+
 
 
         }
